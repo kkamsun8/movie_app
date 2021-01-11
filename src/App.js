9aeb27765d271b2680221209f6c94479062ee1af
kkamsun8 <kkamsun8@gmail.com>
2021-01-11 18:32:51 +0900

    5강까지

--- src/App.js
+++ src/App.js
@@ -1,71 +1,49 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 
- class App extends React.Component{
-   state = {
-     isLoading: true,
-     movies:[]
-   };
-   componentDidMount(){
-    //  setTimeout(()=>{
-    //    this.setState({isLoading: false});
-    //  },6000);
-    axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
-     
-   }
-   render(){
-     const {isLoading} = this.state;
-     return (
-       <div>
-         {isLoading ? "Loading..." : "We are ready"}
-       </div>
-     )
-   }
- }
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
 
+  }
+  componentDidMount() {
+    this.getMovies();
 
-// const foodILike = [
-//   {
-//     id : 1,
-//     name : "kimchi",
-//     image : "http://www.kimchi21.com/good_image/1685371236_s_1.jpg",
-//     rating : 3
-//   },
-//   {
-//     id : 2,
-//     name : "비빔밥",
-//     image : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/220px-Dolsot-bibimbap.jpg",
-//     rating : 2
-//   },
-//   {
-//     id : 3,
-//     name : "칼국수",
-//     image : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Korean.noodle-Kalguksu-01.jpg/220px-Korean.noodle-Kalguksu-01.jpg",
-//     rating : 5
-//   }
-// ];
-
-// function Food(props) {
-//   return (
-//   <>
-//   <h1>I like {props.name}</h1>
-//   <h4>{props.rating}</h4>
-//   <img src={props.image} alt={props.name}/>
-//   </>);
-// }
-
-//  Food.propTypes ={
-//    name: PropTypes.string.isRequired,
-//    image : PropTypes.string.isRequired,
-//    rating : PropTypes.number.isRequired
-//  };
-
-// function App() {
-//   return <div>
-//     <h1>Hello</h1>
-//     {foodILike.map(arg => <Food key={arg.id} name={arg.name} image={arg.image} rating={arg.rating}/>)}
-//   </div>;
-// }
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading
+          ? (<div className="loader">
+            <span className="loader__test">Loading...</span>
+          </div>
+          ) : (
+            <div className="movies">
+              {movies.map(movie =>
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              )}
+            </div>
+          )}
+      </section>
+    )
+  }
+}
 
 export default App;
